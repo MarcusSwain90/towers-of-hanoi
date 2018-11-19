0cd4e40dd6a3b40db91f3ab2d1ff09e30b01925a
Marcus Swain <marcusswain@Marcuss-MacBook-Pro.local>
2018-11-19 18:21:17 -0500

    another commit

--- main.js
+++ main.js
@@ -15,22 +15,29 @@ moveDisc
 // printBoard function is to give visual of where the discs are placed on the board
 
 
-var game = function () {
-    var board =  [
-        [5, 4, 3, 2, 1],
-        [],
-        []
-    ]
-
-    var printBoard = function () {
-        for (var i = 0; i < board.length; i++) {
-            console.log(board[i]);
-        }
-
-    var postBoard
-
-return {
-    print: printBoard
+var board = [
+  [5, 4, 3, 2, 1],
+  [],
+  []
+];
+//Identifies the state of the board
+var printBoard = function() {   //setting up function
+    board.map(function(peg) { //mapping the board with the .map function making the peg a parameter
+    console.log("---" + peg) //console logging the peg to the board
+  })
 }
-    }
+
+var moveDisc = function(startPeg, endPeg) { //takes an argument of the startpeg to the endpeg
+  var removedItem = board[startPeg].pop(); //uses the pop method to pull off of the startPeg
+
+  var endPegArray = board[endPeg] //sets up ending disc move on the array to show on the board
+  var lastItemEndPegArray = endPegArray[endPegArray.length - 1];
+
+  if (removedItem < lastItemEndPegArray || endPegArray.length === 0) { //sets the conditional statement so that a larger disc cannot be stacked on a smaller disc
+     return board[endPeg].push(removedItem);
+  } else {
+    console.log("cannot move");
+  }
+printBoard();
+}
 };
